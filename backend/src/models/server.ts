5afb60ffef54e29ab053714c49457e446575919f
Carlos Carvajal Vazquez <carlos12150@hotmail.com>
2022-03-21 22:34:43 -0600

    feat: first steps api config

--- backend/src/models/server.ts
+++ backend/src/models/server.ts
@@ -1,5 +1,10 @@
-import express, { Application, Request, Response, Router } from "express";
-import cors from "cors";
+import express, { Application, Request, Response, Router } from 'express';
+import cors from 'cors';
+import path from 'path';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import hpp from 'hpp';
+import compression from 'compression';
 
 class Server {
 
@@ -7,46 +12,76 @@ class Server {
   private port: string;
 
   constructor() {
+    this.handleRareEvents();
     this.app = express();
-    this.port = process.env.PORT || "3000";
+    this.port = process.env.PORT || '3000';
 
     console.clear();
-    console.log("\n\n=====================================================================".cyan);
-		console.log("======================== PRICE-TRACKER SERVER =======================".magenta);
-		console.log("=====================================================================\n".cyan);
+    console.log('\n\n=====================================================================');
+    console.log('======================== PRICE-TRACKER SERVER =======================');
+    console.log('=====================================================================');
 
-    
-    this.dbConnection(); // enables DB connection
+
     this.middlewares(); // enables middlewares for requests
+    this.dbConnection(); // enables DB connection
     this.routes(); // enable API routes
   }
 
-  listen(): void {
-    this.app.listen( this.port, () => {
-      console.log( `[!] Server running on port ${this.port.green}.\n` );
-      console.log( "=====================================================================\n".gray );
-    } );
+  handleRareEvents() {
+    process.on('uncaughtException', (err) => {
+      console.log(err.name, err.message, err);
+      process.exit(1); // code 1 is for unhandled rejection, 0 means success
+    });
   }
 
-  dbConnection(): void {}
+  listen(): void {
+    this.app.listen(this.port, () => {
+      console.log(`[!] Server running on port ${this.port}.\n`);
+      console.log('=====================================================================\n');
+    });
+  }
+
+  dbConnection(): void { }
 
   middlewares(): void {
-    this.app.use( cors() );
-    this.app.use( express.json() );
+    this.app.use(cors());
+    this.app.use(express.json());
+    this.app.use(express.static(path.join(__dirname, 'public')));
 
-    // serve public folder
-    // this.app.use( express.static( "" ) )
+    this.app.use(express.static(__dirname, { dotfiles: 'allow' }));
+
+    if (process.env.NODE_ENV === 'development') {
+      this.app.use(morgan('dev'));
+    }
+
+    this.app.use(helmet()); // security http, this already includes kind of protection against xss
+    this.app.use(express.json({ limit: '10kb' })); // limit json size
+    this.app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+
+    // prevent parameter polution
+    this.app.use(
+      hpp({
+        whitelist: ['department', 'limit', 'offset'],
+      })
+    );
+
+    this.app.use(compression()); // reduce data of queries
+
+    this.app.use((req: any, res, next) => {
+      req.requestTime = new Date().toISOString();
+      next();
+    });
   }
 
   routes(): void {
     const router = Router();
-    this.app.use( "/api", router.get( "/", (req, res) => {
+    this.app.use('/api/v1/test', router.get('/', (req, res) => {
       res.json({
         ok: true,
-        msg: "Hello, world!"
+        msg: 'Hello, world!'
       });
-    } ) );
-    
+    }));
+
   }
 
 }
