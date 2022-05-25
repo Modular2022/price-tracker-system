9f411049aa03a5f12f33ecdedf0554ee00c95c42
launderex619 <carlos12150@hotmail.com>
2022-05-24 21:02:28 -0500

    handled email server and error handling

--- backend/src/interfaces/email-transport.i.ts
+++ backend/src/interfaces/email-transport.i.ts
@@ -1,10 +1,14 @@
 import { Options } from "nodemailer/lib/smtp-transport";
 
 export default interface IEmailTransport extends Options {
-  host: string;
-  port: number;
+  host?: string;
+  port?: number;
+  service?: string;
   auth: {
-    user: string;
-    pass: string;
+    user?: string;
+    pass?: string;
+    api_key?: string;
+    DEFAULT_FROM_EMAIL?: string;
+    EMAIL_FROM?: string;
   },
 }
--- backend/src/utils/catch-async.ts
+++ backend/src/utils/catch-async.ts
@@ -1,12 +1,18 @@
 import { RequestHandler } from "express";
 
-export default (fn: Function): RequestHandler => {
-  const routeHandler: RequestHandler = (req, res, next) => {
-    try {
-      fn(req, res, next);
-    } catch (err) {
-      next(err);
-    }
+// export default (fn: Function): RequestHandler => {
+//   const routeHandler: RequestHandler = (req, res, next) => {
+//     try {
+//       fn(req, res, next);
+//     } catch (err) {
+//       next(err);
+//     }
+//   };
+//   return routeHandler;
+// }
+
+export default (fn: any) => {
+  return (req: any, res: any, next: any) => {
+    fn(req, res, next).catch(next);
   };
-  return routeHandler;
-}
+};
