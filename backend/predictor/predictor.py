1de74809e362a63aa9d0ac0de1657abdecff2d26
Luis Medina <luis.m.medina@oracle.com>
2022-05-25 03:18:25 -0500

    asdasfsdfasdf

--- backend/predictor/predictor.py
+++ backend/predictor/predictor.py
@@ -1,36 +1,66 @@
-#!/usr/bin/env /usr/bin/python3
-from matplotlib import pyplot
-from pandas.plotting import lag_plot
+from statsmodels.tsa.arima.model import ARIMA
+import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 
-def predict_product_price(asin):
-    pass
+def plot_df(df, x, y, title="", xlabel='Date', ylabel='Value', dpi=100):
+    plt.figure(figsize=(16, 5), dpi=dpi)
+    plt.plot(x, y, color='tab:red')
+    plt.gca().set(title=title, xlabel=xlabel, ylabel=ylabel)
+    plt.show()
 
-# Return DF
+
 def preprocess_data(filename, nrows=100):
     # Read CSV price history file
-    df = pd.read_csv(filename, nrows=nrows)
-    
+    df = pd.read_csv(filename, nrows=nrows, parse_dates=['date'])
+    # df = pd.read_csv(filename, parse_dates=['date'], squeeze=True)
     # Transform date format to just date (01-01-12)
     df['date'] = pd.to_datetime(df['date']).dt.date
+    # # Use date as the index
+    df.set_index('date', inplace=True)
 
-    # Use date as the index
-    df.set_index('date', inplace = True)
+    df.index = pd.DatetimeIndex(df.index).to_period('M')
 
     return df
 
-def train_product_model(filename):
-    df = preprocess_data(filename)
 
-    # series.plot()
-    # pyplot.show()
+def train_model(series):
+    model = ARIMA(series, order=(2, 0, 1))
+    model_fit = model.fit()
 
-    # lag_plot(series)
-    # pyplot.show()
+    # summary of fit model
+    model_fit.summary()
+
+    return model_fit
+
+
+def predict_price(series, model_fit):
+    inicial = series.price[len(series)-1]
+    final = model_fit.predict(len(series), len(series))
+    final = final.values[0]
+    val = inicial < final    
+    percentile = ((final - inicial)/inicial)
+    return {
+        'goesUp': str(val).lower(),
+        'value': str(final),
+        'percentage': str(percentile)
+    }
+
+
+def main():
+    series = preprocess_data('training_data/prices.csv')
+    model = train_model(series)
+    json_obj = predict_price(series, model)
+    print(json_obj)
+
+    # plot_df(series, x=series.index, y=series.price, title='Sales')
 
     # from pandas.plotting import autocorrelation_plot
     # autocorrelation_plot(series)
 
-    # from statsmodels.graphics.tsaplots import plot_acf
-    # plot_acf(series, lags=31)
+    # train = series[series.index < pd.to_datetime("2022-04-01", format='%Y-%m-%d')]
+    # test = series[series.index > pd.to_datetime("2022-04-02", format='%Y-%m-%d')]
+
+if __name__ == '__main__':
+    main()
